99dc2733b0a1a9d0e324708ccf4d95e3884d60ef
comepty-official <comepty.org@gmail.com>
2026-07-21 11:56:04 +0100

    Update serializers.py

--- backend/chatapp/serializers.py
+++ backend/chatapp/serializers.py
@@ -1,31 +1,28 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework import serializers
-from django.contrib.auth.models import User
+
 from .models import FriendRequest, Conversation, Message
 
 
-
-
 class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=False,
+        allow_blank=True,
+    )
+
     class Meta:
         model = User
         fields = ["username", "email", "password"]
         extra_kwargs = {
-            "password": {"write_only": True}
+            "password": {"write_only": True},
         }
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        return User.objects.create_user(
             username=validated_data["username"],
-            email=validated_data["email"],
+            email=validated_data.get("email", ""),
             password=validated_data["password"],
         )
-        return user
-
-
-
-
 
 
 class UserSerializer(serializers.ModelSerializer):
